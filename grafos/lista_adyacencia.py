3c28d054d1d4b29cd53dd13c7ef0601982b35f8b
Iñigo Quintana <quintanadinigo@gmail.com>
2023-07-03 12:42:18 -0600

    Agrega el peso de los nodos a una matriz

--- grafos/lista_adyacencia.py
+++ grafos/lista_adyacencia.py
@@ -2,23 +2,25 @@
 #22/06/2023
 
 from cola import Queue
+import numpy as np
 #from ..estructurasLineales.cola import Queue
 
 
 class Node:
-    def __init__(self, data, weight=0):
+    def __init__(self, data):
         self.vertex = data
         self.next = None
-        self.weight = weight
 
 
 class Graph:
     def __init__(self, size):
         self.size = size #Tamaño del gráfico
-        self.graph = {}
+        self.graph = {} #Diccionario con el grafo
         self.keys = [] #Nodos enm
+        self.matriz = np.array([[0 for j in range(size)] for i in range(size)])
         # print(self.graph)
         
+
     def add_vertex(self, data):
         if data not in self.graph and len(g.graph) < self.size:
             self.graph[data] = []
@@ -30,17 +32,18 @@ class Graph:
         print(self.graph)
 
 
-    def add_edge(self, v , w, weight=0):
+
+    def add_edge(self, v , w, peso=1):
         #TODO: insertar lista adyacentes
 
         if v in self.keys and w in self.keys:
             #*Agregar en diccionario vacío
             if self.graph[v] == []:
-                self.graph[v] = Node(w, weight)
+                self.graph[v] = Node(w)
 
 
             if self.graph[w] == []:   
-                self.graph[w] = Node(v, weight)
+                self.graph[w] = Node(v)
                 
                 
             #*Agregar diccionario existente
@@ -52,7 +55,7 @@ class Graph:
                     ultimo_nodo = ultimo_nodo.next
 
                 if ultimo_nodo.vertex != w:
-                    ultimo_nodo.next = Node(w, weight)
+                    ultimo_nodo.next = Node(w)
 
             
             if self.graph[w] != []:
@@ -62,9 +65,11 @@ class Graph:
                     ultimo_nodo = ultimo_nodo.next
 
                 if ultimo_nodo.vertex != v:
-                    ultimo_nodo.next = Node(v, weight)
-           
-            
+                    ultimo_nodo.next = Node(v)
+        
+            self.matrizPeso(v, w, peso)
+
+
 
     def recorrido(self, inicial, visitados=list(), nodos=Queue()):
         """Recorrido del grafo en  forma de DFS"""
@@ -129,7 +134,7 @@ class Graph:
     def display(self, key):
         if key in self.keys:
             
-            print(f"\nEl nodo {key} conecta con: ", end='')
+            print(f"\n{key} : ", end='')
             x = self.graph[key]
 
             while x != None:
@@ -139,19 +144,31 @@ class Graph:
         else:
             print("\nNo se encuentra la llave :(")
 
-    
-        
+
+    def matrizPeso(self, v, w, peso):
+        x = self.keys.index(v)
+        y = self.keys.index(w)
+
+        if x >= 0 and y >= 0:
+            self.matriz[x][y] = peso
+            self.matriz[y][x] = peso
+
+        else:
+            print("No se puede añadir en esta ubicación")
+
+
+    def mostrar(self):
+        print(self.keys)
+        for i in self.matriz:
+            for j in i:
+                print(j, end='  ')
+            print()
+
+
 
 #? -- Main --
 
 g = Graph(9)
-
-
-# g.add_vertex(0)
-# g.add_vertex(1)
-# g.add_vertex(2)
-# g.add_vertex(3)
-# g.add_vertex(4)
 
 g.add_vertex('A')
 g.add_vertex('B')
@@ -169,11 +186,11 @@ g.add_edge('A','B')
 g.add_edge('A','D')
 g.add_edge('A','G')
 
-g.add_edge('B','C')
+g.add_edge('B','C', 8)
 g.add_edge('B','G')
 
-g.add_edge('C','I')
 g.add_edge('C','H')
+g.add_edge('C','I')
 
 g.add_edge('D','E')
 g.add_edge('D','G')
@@ -187,21 +204,27 @@ g.add_edge('F','I')
 g.add_edge('I','H')
 
 
-# g.add_edge(0, 1)
-# g.add_edge(0, 2)
-# g.add_edge(0, 3)
-# g.add_edge(2, 1)
-# g.add_edge(2, 4)
 
+g.display('A')
+g.display('B')
 g.display('C')
+g.display('D')
+g.display('E')
+g.display('F')
+g.display('G')
+g.display('H')
+g.display('I')
 
 print("\nRecorrido")
-g.recorrido('A')
+#g.recorrido('A')
 
 
 print("\n\nRecorrido Nivel")
-g.recorridoNivel('A')
+#g.recorridoNivel('A')
 
-#TODO Bombardeen Tijuana 
-#todo bungou stray dogs
+print()
+g.mostrar()
+
+# ToDo Bombardeen Tijuana 
+# ToDo bungou stray dogs
 # ToDo Dijkstra
