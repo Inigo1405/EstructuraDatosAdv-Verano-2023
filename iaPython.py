453af7702986b3a02600c5488d7edd971e26fb18
Iñigo Quintana <quintanadinigo@gmail.com>
2023-07-04 11:22:51 -0600

    Bases a estudiar para IAs en python

---
+++ iaPython.py
@@ -0,0 +1,27 @@
+# 04/07/2023
+# Iñigo Quintana Delgadillo
+
+#! Estudiar para IA
+#pep8
+#numpy
+#pandas
+#TensorFlow
+
+#Redes neuronales
+#Machine Learning
+#Aprendizaje profundo (PyTorch)
+
+#Datos kaggle
+
+
+#*Terminos base:
+# Agent: Entity that percives its environment and acts upon that enviroment
+# State: A configuration of the agent and its enviroment. (Variaciones)
+# Initial State: The state in which the agent begins
+# Actions: Choices that can be made in a state.
+# Transition model: A description of what state results from performing any applicable 
+#                   action in any state.
+# Statepace: The set of all states reachable from the initial state by any sequence of actions.
+
+
+
