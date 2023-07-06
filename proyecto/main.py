58c8bf47348974a6cca2289ccc50b3c2276011a8
Iñigo Quintana <quintanadinigo@gmail.com>
2023-07-05 19:45:24 -0600

    proyecto final, con interfaz gráfica

--- proyecto/main.py
+++ proyecto/main.py
@@ -5,11 +5,27 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.Display.clicked.connect(self.button_clicked)
-        #self.statusbar.showMessage("Ready!")
+        self.display.clicked.connect(self.button_clicked)
+        self.statusbar.showMessage("Ready!")
+        self.x = 0
+        self.y = 0
+        
         
     def button_clicked(self):
         print("Hola Mundo!")
+        
+        
+    def show_pos(self, click):
+        print(f"Left button pressed")
+        
+        
+    def mouseEvent():
+        pass
+    
+    
+    def paintEvent(self, event):
+        pass
+    
         
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
